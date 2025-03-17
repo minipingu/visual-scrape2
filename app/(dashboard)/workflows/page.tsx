861ef48c0356d5a78daaab70a94eb8d8c33bae1a
minipingu <ranmaniax@gmail.com>
2025-03-17 22:52:19 +0700

    create workflow dialog

--- app/(dashboard)/workflows/page.tsx
+++ app/(dashboard)/workflows/page.tsx
@@ -3,6 +3,7 @@ import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 import { Skeleton } from '@/components/ui/skeleton'
 import { AlertCircle, InboxIcon } from 'lucide-react'
 import React, { Suspense } from 'react'
+import CreateWorkflowDialog from './_components/CreateWorkflowDialog'
 
 function PageWorkflows() {
 	return (
@@ -12,6 +13,7 @@ function PageWorkflows() {
 					<h1 className='text-3xl font-bold'>Workflows</h1>
 					<p className='text-muted-foreground'>Manage your workflows</p>
 				</div>
+				<CreateWorkflowDialog />
 			</div>
 			<div className='h-full py-6'>
 				<Suspense fallback={<UserWorkflowsSkeleton />}>
@@ -58,6 +60,7 @@ async function UserWorkflows() {
 						Click the button below to create your first workflow
 					</p>
 				</div>
+				<CreateWorkflowDialog triggerText='Create your first workflow' />
 			</div>
 		)
 	}
