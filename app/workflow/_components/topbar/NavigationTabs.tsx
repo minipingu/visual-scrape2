386015e3f5d92856bbb41d08039591e7dae5aaf7
minipingu <ranmaniax@gmail.com>
2025-06-05 05:57:06 +0700

    ok fixed [3] to [2] just wrong array

--- app/workflow/_components/topbar/NavigationTabs.tsx
+++ app/workflow/_components/topbar/NavigationTabs.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 
 function NavigationTabs({ workflowId }: { workflowId: string }) {
 	const pathname = usePathname()
-	const activeValue = pathname?.split('/')[3]
+	const activeValue = pathname?.split('/')[2]
 	return (
 		<Tabs value={activeValue} className='w-[400px]'>
 			<TabsList className='grid w-full grid-cols-2'>
